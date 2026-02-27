99f86ec854fa1e591235ec1e79a11a42e6e39752
luiscarlos <luiscarlos8pe@gmail.com>
2026-02-27 18:51:53 -0300

    Interfaces pt 1

---
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/teste/DateLoaderTeste01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.teste;
+
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DateBseLoader;
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
+
+public class DateLoaderTeste01 {
+    public static void main(String[] args) {
+        DateBseLoader dateBseLoader = new DateBseLoader();
+        FileLoader fileLoader = new FileLoader();
+
+        dateBseLoader.load();
+        fileLoader.load();
+    }
+}

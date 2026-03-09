15989df5cb64ad0b671daf38d27283a08a27ba7b
luiscarlos <luiscarlos8pe@gmail.com>
2026-03-09 15:58:26 -0300

    Polimorfismo pt 4

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -7,11 +7,16 @@ import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 public class CalculadoraImposto {
 
 
-    public static void calcularImposto(Produto produto){
+    public static void calcularImposto(Produto produto) {
         System.out.println("Relatório de imposto");
-        double imposto  = produto.calcularImposto();
+        double imposto = produto.calcularImposto();
         System.out.println("Produto: " + produto.getNome());
         System.out.println("Preço: " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
+
+        if (produto instanceof Tomate) {
+            Tomate tomate = (Tomate) produto;
+            System.out.println("Tomate: " + tomate.getDataValidade());
+        }
     }
 }

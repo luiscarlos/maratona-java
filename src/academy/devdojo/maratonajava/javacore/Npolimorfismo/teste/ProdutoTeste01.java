e341b44d795399b4fff91347b02d4b79d1b0b31e
luiscarlos <luiscarlos8pe@gmail.com>
2026-03-09 13:42:52 -0300

    Polimorfismo pt 3

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/teste/ProdutoTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/teste/ProdutoTeste01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.teste;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTeste01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10i7", 11000);
         Tomate tomate = new Tomate("Tomate Siciliano", 10);
+        Televisao tv = new Televisao("Snsung", 2000);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("-------------------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("-------------------------------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }

56ac460fc5d203149f1ff38771aeb119ba8a345d
luiscarlos <luiscarlos8pe@gmail.com>
2026-03-12 15:51:23 -0300

    Exceções parte 01

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/erro/teste/StackOverflowTest01.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Oexception.erro.teste;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/teste/RumtimeExceptionTeste01.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.teste;
+
+public class RumtimeExceptionTeste01 {
+    public static void main(String[] args) {
+
+    }
+}
